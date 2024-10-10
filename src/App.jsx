2f9daf9aa5ff0e36a8abeaad1d6e9987a6d71764
Toiste <marllon.ribeiro027@gmail.com>
2024-10-10 17:19:17 -0300

    update video

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { useEffect, useState } from 'react';
 import './App.css'
-import videoJoice from "./assets/videoNiverJoice.mp4"
-import poster from "./assets/capaVideo.png"
 import planta from "./assets/planta.png"
 
 function App() {
@@ -60,20 +58,23 @@ function App() {
   return (
     
     <main className='flex relative w-screen  bg-[#202020] flex-col min-h-screen pb-[70px]'>
-      <header className='w-full h-[109px] bg-[#D30000] pt-[49px] flex justify-center items-center gap-[12px]'>
+      <header className='w-full h-[109px] bg-[#D30000] flex justify-center items-center gap-[12px]'>
         <img src="./whiteHeart.svg" alt="coração branco" className='w-[51px]' />
         <h2 >MINHA MELHOR ESCOLHA FOI VOCÊ!</h2>
       </header>
       <div className="w-full flex justify-center items-center items-center h-full flex-col gap-[17px] pt-[50px]">
-      <video controls poster={poster} width="318" height="511" className="rounded-[10px] border-solid border-[2px] border-[#FFFFFF]/65">
-        <source src={videoJoice} type="video/mp4" />
-        Seu navegador não suporta o vídeo.
-      </video>
+      <iframe width="318" height="511" src="https://www.youtube.com/embed/vgvhkaHFFOo?si=LnrNfMv1Kb4eRo1k"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+        className=" rounded-[10px] border-solid border-[2px] border-[#FFFFFF]/65"
+        ></iframe>
       <h1 className='text-[24px]'>Nosso Tempo Juntos</h1>
       <p >{years} ano, {months} meses, {days} dias</p>
       <p>{hours} horas, {minutes} minutos, {seconds} segundos</p>
       </div>
-      <img src={planta} alt="planta divisória de seção" className='w-full absolute bottom-[-90px] z-[1]' />
     </main>
   )
 }
